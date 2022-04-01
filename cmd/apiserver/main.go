62f8bdb00e09fd77f74c256a50aab71660ab1268
KoLLlaka <kotshredengera@gmail.com>
2022-04-01 14:29:46 +0300

    config file

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"github.com/BurntSushi/toml"
+	"github.com/KoLLlaka/http-rest-api/internal/app/apiserver"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KoLLlaka/http-rest-api
 
 go 1.18
+
+require github.com/BurntSushi/toml v1.0.0
